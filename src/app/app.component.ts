602c6c4e8715452816f6bb70b496a4ee76a7898a
Chamith Priyankara <chamithg@live.com>
2021-08-30 19:01:34 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dating-client';
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) {
+
+  }
+  title = "Dating App";
+  users: any;
+ 
+  
+  ngOnInit() {
+    
+    this.http.get("https://localhost:44385/api/users").subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error)
+    })
+  }
 }
